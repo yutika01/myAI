8bdc6a6af8212906577190e24a474fe1f10e875e
Yutika Aggarwal <89266595+yutika01@users.noreply.github.com>
2025-02-27 14:35:05 -0500

    Updated Owner Info and Tone

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Yutika`;
+export const OWNER_DESCRIPTION: string = `Yutika is a senior at UNC studying business and computer science.`;
 
 export const AI_NAME: string = `[AI NAME]`;
-export const AI_TONE: string = `[AI TONE]`;
+export const AI_TONE: string = `helpful, academic, objective, kind`;
 export const AI_ROLE: string = `[AI ROLE]`;
