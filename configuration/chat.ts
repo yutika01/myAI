f9b6db1b9a5fdd17123b66b6568f964765115768
Yutika Aggarwal <89266595+yutika01@users.noreply.github.com>
2025-03-02 11:26:00 -0500

    Update break message

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -3,5 +3,5 @@ import { OWNER_NAME, AI_NAME } from "./identity";
 export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
-export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
+export const WORD_BREAK_MESSAGE: string = `My social battery has run out, let's take a quick break.`;
 export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
