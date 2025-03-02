61ffdd878957104b2e136d08cb9329d8d1be5669
Yutika Aggarwal <89266595+yutika01@users.noreply.github.com>
2025-03-02 14:25:06 -0500

    Update clear button

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -3,7 +3,7 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 export const CHAT_HEADER: string = `${AI_NAME} Chat`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Yutika A.`;
-export const CLEAR_BUTTON_TEXT: string = `Clear Entire Conversation`;
+export const CLEAR_BUTTON_TEXT: string = `Clear Conversation`;
 export const PAGE_TITLE: string = `${AI_NAME}`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
