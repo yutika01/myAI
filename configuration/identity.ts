66aba0f5b4981d6c2827c583ffff0d325abba5a8
Yutika Aggarwal <89266595+yutika01@users.noreply.github.com>
2025-03-02 11:34:20 -0500

    Update AI name, role, tone

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Yutika`;
-export const OWNER_DESCRIPTION: string = `Yutika is a senior at UNC studying business and computer science.`;
+export const OWNER_DESCRIPTION: string = `Yutika is a senior at UNC studying business and computer science, with an avid interest in listening to music.`;
 
-export const AI_NAME: string = `[AI NAME]`;
-export const AI_TONE: string = `helpful, academic, objective, kind`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_NAME: string = `BeatBot`;
+export const AI_TONE: string = `helpful, academic, objective, kind, music-oriented`;
+export const AI_ROLE: string = `To give song and music recommendations to users based on provided information`;
