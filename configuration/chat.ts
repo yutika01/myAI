558590d9b3ddfcf1672164018ed34a43d596184d
Yutika Aggarwal <89266595+yutika01@users.noreply.github.com>
2025-03-04 23:47:08 -0500

    Update initial and break message

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,7 +1,9 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant. How can I assist you today?`;
+export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, a Sustainability Guru. I am trained on ethical sourcing & sustainability 
+compliance. The Earth does not belong to us; we belong to the Earth – let’s make sure we’re treating it right! 
+How can I help you in the process?`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
-export const WORD_BREAK_MESSAGE: string = `My social battery has run out, let's take a quick break.`;
+export const WORD_BREAK_MESSAGE: string = `Just like nonrenewable energy, I also run out of power. Let's take a quick break.`;
 export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
