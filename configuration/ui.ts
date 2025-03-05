29d0dafb34657d149c07351df1df1ecda24eae44
Yutika Aggarwal <89266595+yutika01@users.noreply.github.com>
2025-03-04 23:56:15 -0500

    Update footer, clear button, page description

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -2,9 +2,9 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `${AI_NAME} Chat`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `Customized AI by Yutika A.`;
-export const CLEAR_BUTTON_TEXT: string = `Clear Conversation`;
+export const FOOTER_MESSAGE: string = `Customized AI by Yutika A. Please refer to a licensed attorney for any legal advice.`;
+export const CLEAR_BUTTON_TEXT: string = `Start Over`;
 export const PAGE_TITLE: string = `${AI_NAME}`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const PAGE_DESCRIPTION: string = `${AI_NAME}: Ethical Sourcing & Sustainability Compliance Assistant`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
